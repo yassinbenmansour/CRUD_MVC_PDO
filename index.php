ae22170f938fa002ec8065cb4a263ed4a198ec11
yassinbenmansour <benmansour.yassinne@gmail.com>
2023-05-31 01:41:41 +0100

    update

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title><?php echo  "HOME" ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
 
 </head>
+
 <body>
-    <h1> CRUD PDO  MVC</h1>
+    <h1> CRUD PDO MVC</h1>
     <a href="controller.php?cmd=list">list student</a>
     <a href="controller.php?cmd=create">add new student</a>
 </body>
+
 </html>
